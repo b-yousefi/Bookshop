1e6dfcf117ab6352e7b6bcbf0836bbf8c4790cbb
behnaz <b.yousefi2911@gmail.com>
2020-12-03 18:52:34 +0330

    [refactor] moving api folder to separate repositories
    spring: https://github.com/b-yousefi/bookshop-spring
    Django: https://github.com/b-yousefi/bookshop-django

--- api/spring/bookshop/src/test/resources/b_yousefi/bookshop/integrated_tests/create_publication.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO publication
-(id,
- description,
- name,
- website)
-VALUES (1,
-        'Oxford University Press (OUP) is the largest university press in the world and the second oldest after Cambridge University Press',
-        'oxford',
-        'www.oxford.com');
